dd51391305058d2b628617c064fba898ca6626d9
Jarlath Holleran <jarlath.holleran@gmail.com>
2020-12-13 21:20:07 +0000

    Added template of tests for the subscriber

---
+++ test/subscriber.test.js
@@ -0,0 +1,10 @@
+'use strict'
+
+test('Discover the Hub and Topic given a resources URL', () => {
+});
+
+test('Subscribe to updates at the hub', () => {
+});
+
+test('Accepts content distribution requests from the hub', () => {
+});
